705a445b5a676d586ce5e686a6d0acaedf680d13
MB S <dds@raubtierbaendiger.de>
2018-12-27 12:01:05 +0000

    Deploy from Forestry.io

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function() {
+  $('a[href="#standard-edition"]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+var mainMenu = document.querySelector(".main-menu");
+// On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  mainMenu.classList.toggle("is-active");
+  // Do something else, like open/close menu
+});
